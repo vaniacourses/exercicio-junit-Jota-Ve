e97c477fab03158361c9791799a5f0cda2d5f14f
João Vitor Barbosa <j.vs.b13@gmail.com>
2025-09-05 22:39:38 -0300

    CalculadoraTest

--- test/calculadora/CalculadoraTest.java
+++ test/calculadora/CalculadoraTest.java
@@ -1,6 +1,7 @@
 package calculadora;
 
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -13,41 +14,83 @@ import org.junit.jupiter.api.Test;
 
 @DisplayName("Classe para teste da calculadora")
 public class CalculadoraTest {
-	
+
 	private Calculadora calc;
-	
+
 	@BeforeEach
 	public void inicializa() {
 		calc = new Calculadora();
 	}
-	
-	@DisplayName("Testa a soma de dois números")
+
+	@DisplayName("Testa a soma de dois nÃºmeros")
 	@Test
 	public void testSomaDoisNumeros() {
-		int soma = calc.soma(4, 5);		
-		Assertions.assertEquals(9, soma);		
+		int soma = calc.soma(4, 5);
+		Assertions.assertEquals(9, soma);
 	}
-	
+
 	@Test
 	public void testDivisaoDoisNumeros() {
 		int divisao = calc.divisao(8, 4);
 		assertTrue(divisao == 2);
 	}
-	
+
 	@Test
 	public void testDivisaoPorZero() {
 		try {
 			int divisao = calc.divisao(8, 0);
-			fail("Exceção não lançada");
+			fail("ExceÃ§Ã£o nÃ£o lanÃ§ada");
 		}catch (ArithmeticException e) {
 			assertEquals("/ by zero", e.getMessage());
-		}		
+		}
 	}
-	
+
 	@Test
 	public void testDivisaoPorZeroComAssertThrows() {
 		assertThrows(ArithmeticException.class,
 				() -> calc.divisao(8, 0));
+	}
+
+	@Test
+	public void testSomatoria(){
+		int somatoria = calc.somatoria(3);
+		assertEquals(somatoria, 6);
+	}
+
+	@Test
+	public void testSomatoriaZero(){
+		int somatoria = calc.somatoria(0);
+		assertEquals(somatoria, 0);
+	}
+
+	@Test
+	public void testEhPositivo(){
+		assertTrue(calc.ehPositivo(1));
+	}
+
+	@Test
+	public void testZeroEhPositivo(){
+		assertTrue(calc.ehPositivo(0));
+	}
+
+	@Test
+	public void testNaoEhPositivo(){
+		assertFalse(calc.ehPositivo(-1));
+	}
+
+	@Test
+	public void testComparaIgualdade(){
+		assertEquals(calc.compara(5, 5), 0);
+	}
+
+	@Test
+	public void testComparaMenorComMaior(){
+		assertEquals(calc.compara(1, 2), -1);
+	}
+
+	@Test
+	public void testComparaMaiorComMenor(){
+		assertEquals(calc.compara(7, 0), 1);
 	}
 
 }
